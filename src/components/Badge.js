09d2a2e009f57d4229545b5ef9c6662aba6f9779
José Francisco Díaz <jfcodiaz@gmail.com>
2019-03-28 13:12:47 -0600

    compoente styles

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import confLogo from '../images/badge-header.svg';
-
+import "../styles/Badge.css"
 class Badge extends React.Component {
   render() {
     return (
-      <div>
-        <div>
+      <div className="Badge">
+        <div className="Badge__header">
           <img src={confLogo} alt="Logo"/>
         </div>
-        <div>
-          <img src="https://www.gravatar.com/avatar?d=identicon" alt="Avatar"/>
-          <h1>Richard <br/> Kaufman</h1>
+        <div className="Badge__section-name">
+          <img className="Badge__avatar" src="http://gravatar.com/avatar/001650e253ed06b724d0e8eae545c6db?s=120" alt="Avatar"/>
+          <h1>Pako <br/> Díaz</h1>
         </div>
-        <div>
-          <p>Fronted Enginner</p>
-          <p>@sparragus</p>
+        <div className="Badge__section-info">
+          <h3>Full Stack</h3>
+          <div>@sparragus</div>
         </div>
-        <div>
+        <div className="Badge__footer">
           #platziconf
         </div>
       </div>
