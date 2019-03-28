538c4e7ce1c8bdcc838d217c0ed2605f3d70ac25
José Francisco Díaz <jfcodiaz@gmail.com>
2019-03-28 15:03:21 -0600

    state and forms introduction

--- src/components/BadgeForm.js
+++ src/components/BadgeForm.js
@@ -1,22 +1,24 @@
 import React from 'react'
 
 class BadgeForm extends React.Component {
+  
+  state = {}
 
-  handleChange(e) {
-    console.log({
-      name: e.target.name,
-      value: e.target.value
+  handleChange = e => {
+    this.setState({
+      [e.target.name] : e.target.value
     });
   }
 
-  handleClick(e) {
+  handleClick = e => {
     console.log("Button was clicked")
   }
 
   handleSubmit = e => {
     e.preventDefault();
-    console.log("form was submitted")
+    console.log(this.state)
   }
+
   render () {
     return (
       <div>
@@ -24,8 +26,29 @@ class BadgeForm extends React.Component {
         <form onSubmit={this.handleSubmit} action="">
           <div className="form-group">
             <label>First Name</label>
-            <input onChange={this.handleChange} className="form-control" type="text" name="firstName"/>
+            <input onChange={this.handleChange} className="form-control" type="text" name="firstName" value={this.state.firstName}/>
           </div>
+
+          <div className="form-group">
+            <label>Last Name</label>
+            <input onChange={this.handleChange} className="form-control" type="text" name="lastName" value={this.state.lastName}/>
+          </div>
+
+          <div className="form-group">
+            <label>Email</label>
+            <input onChange={this.handleChange} className="form-control" type="email" name="email" value={this.state.email}/>
+          </div>
+
+          <div className="form-group">
+            <label>Job Title</label>
+            <input onChange={this.handleChange} className="form-control" type="text" name="jobTitle" value={this.state.jobTitle}/>
+          </div>
+
+          <div className="form-group">
+            <label>Twitter</label>
+            <input onChange={this.handleChange} className="form-control" type="text" name="twitter" value={this.state.twitter}/>
+          </div>
+
           <button onClick={this.handleClick} className="btn btn-primary">Save</button>
         </form>
       </div>
